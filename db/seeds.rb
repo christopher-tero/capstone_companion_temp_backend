b1b3a504e49abd4bcfe982a48169371561b8cf95
Christopher Tero <christopher.tero@gmail.com>
2019-07-19 13:24:13 -0600

    some updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  username: "John Doe",
+  password: "password",
+  email: "email@email.me"
+)
+
+Project.create(
+  title: "Project One",
+  description: "Some pretty long description of some fictional project that does not exist yet that will be editable but isn't yet. Also will eventually be able to delete the project or add a new one, etc.",
+  features: "An ordered list of all the amazing features you plan to put in this app",
+  user_id: 1,
+  resources: "List of some resources"
+)
+Project.create(
+  title: "Project Two",
+  description: "Some pretty long description of some fictional project that does not exist yet that will be editable but isn't yet. Also will eventually be able to delete the project or add a new one, etc.",
+  features: "An ordered list of all the amazing features you plan to put in this app",
+  user_id: 1,
+  resources: "List of some resources"
+)
+Project.create(
+  title: "Project Three",
+  description: "Some pretty long description of some fictional project that does not exist yet that will be editable but isn't yet. Also will eventually be able to delete the project or add a new one, etc.",
+  features: "An ordered list of all the amazing features you plan to put in this app",
+  user_id: 1,
+  resources: "List of some resources"
+)
+Project.create(
+  title: "Project Four",
+  description: "Some pretty long description of some fictional project that does not exist yet that will be editable but isn't yet. Also will eventually be able to delete the project or add a new one, etc.",
+  features: "An ordered list of all the amazing features you plan to put in this app",
+  user_id: 1,
+  resources: "List of some resources"
+)
+Project.create(
+  title: "Project Five",
+  description: "Some pretty long description of some fictional project that does not exist yet that will be editable but isn't yet. Also will eventually be able to delete the project or add a new one, etc.",
+  features: "An ordered list of all the amazing features you plan to put in this app",
+  user_id: 1,
+  resources: "List of some resources"
+)
